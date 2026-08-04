f446425b1b7adbcc2ec207b968e4d0569a6c1ea5
sayed <mohammed.ali@asciisys.com>
2026-08-04 13:43:44 +0600

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,15 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Royale Arab Men's Club",
-  description: "Men's club income, expense, customer, employee and report management"
+  description:
+    "Men's club income, expense, customer, employee and report management",
 };
 
 const outfit = Outfit({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={outfit.className}>{children}</body>
