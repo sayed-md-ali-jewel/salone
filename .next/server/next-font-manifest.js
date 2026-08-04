15738e8a880990b7d3348c67540a875a3dde62c0
sayed <mohammed.ali@asciisys.com>
2026-08-04 12:12:57 +0600

    change

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/apple/Desktop/personal/salone-mgt/app/layout":["static/media/7b0b24f36b1a6d0b-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
--- .next/static/chunks/app/(dashboard)/staff-payments/page-aa9d21517ecffad3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[945],{3704:(e,s,n)=>{Promise.resolve().then(n.bind(n,6535)),Promise.resolve().then(n.bind(n,6554)),Promise.resolve().then(n.t.bind(n,2619,23))}},e=>{e.O(0,[619,582,535,441,255,358],()=>e(e.s=3704)),_N_E=e.O()}]);
